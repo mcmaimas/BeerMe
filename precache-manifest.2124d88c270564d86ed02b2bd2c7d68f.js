96877ca035d37b798b64e1927c42ef1db5dd80ab
Sam McMurray <scmcmur@gmail.com>
2020-05-10 12:39:54 -0400

    Updates

---
+++ precache-manifest.2124d88c270564d86ed02b2bd2c7d68f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9793357ebd23ddf1514d0959b7da8db2",
+    "url": "/BeerMe/index.html"
+  },
+  {
+    "revision": "867248e8abdd1a05b230",
+    "url": "/BeerMe/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1fcb50d1e5c6a39b6a60",
+    "url": "/BeerMe/static/css/main.45cd5e57.chunk.css"
+  },
+  {
+    "revision": "867248e8abdd1a05b230",
+    "url": "/BeerMe/static/js/2.f569b34a.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/BeerMe/static/js/2.f569b34a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fcb50d1e5c6a39b6a60",
+    "url": "/BeerMe/static/js/main.afa11366.chunk.js"
+  },
+  {
+    "revision": "b4461cd1604103e6d1ae",
+    "url": "/BeerMe/static/js/runtime-main.9627d5ae.js"
+  },
+  {
+    "revision": "589146d6625df25eaf840fc7cb7d8e9b",
+    "url": "/BeerMe/static/media/sauce.589146d6.png"
+  }
+]);
