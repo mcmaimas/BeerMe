6cbeb4d077957f1ccca2f2ae1a791a741816aafc
Sam McMurray <scmcmur@gmail.com>
2020-05-10 12:24:00 -0400

    Formatting so UI can be displayed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,27 +9,23 @@ function footer(props) {
         onClick={() => props.onClick()}
         name="home"
         color={props.activeTabName === "home" ? "green" : "grey"}
-        style={{ width: "20%", padding: "3%" }}
+        style={{ width: "20%", marginTop: "2%" }}
       />
       <Icon
         onClick={() => props.onClick("search")}
         name="search"
         color={props.activeTabName === "search" ? "green" : "grey"}
-        style={{ width: "20%", padding: "3%" }}
+        style={{ width: "20%", marginTop: "2%" }}
       />
 
       <Icon
         onClick={() => props.onClick("trophy")}
         name="trophy"
         color={props.activeTabName === "trophy" ? "yellow" : "grey"}
-        style={{ width: "20%", padding: "3%" }}
+        style={{ width: "20%", marginTop: "2%" }}
       />
     </div>
   );
 }
 
 export default footer;
-
-// home search archive
-// home search  trophy
-// home search folder-folderopen
